822623b83a911ebfb4cf6250d9167224d3078fe5
esellers25 <erin.e.sellers@gmail.com>
2021-03-22 08:38:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  fetch('https://anapioficeandfire.com/api/books')
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(function(response){
+    return response.json();
+  })
+  .then(function(json){
+    return renderBooks(json)
+  });
+
 }
 
 function renderBooks(books) {
